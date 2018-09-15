0b5a103b3e2a2dfcd56cab56bc551e4841f34695
DE <subashhshrestha10@gmail.com>
2018-09-15 14:11:20 +0545

    3rd commit

---
+++ app/src/main/java/com/example/de/tripshayatri/PlaceInformationModule1.java
@@ -0,0 +1,44 @@
+package com.example.de.tripshayatri;
+
+public class PlaceInformationModule1 {
+    String pid;
+    String name;
+
+    String description;
+
+    public String getPid() {
+        return pid;
+    }
+
+    public void setPid(String pid) {
+        this.pid = pid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+
+    String url;
+}
